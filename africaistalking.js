fc04b3678b443680134cbb7f73a2a4af4ad8d5da
johnanisere <johnanisere@gmail.com>
2018-02-21 17:41:04 +0100

    bug fix

--- africaistalking.js
+++ africaistalking.js
@@ -1,10 +1,12 @@
-const sendMessage=(username,message,to,apikey,from)=>(
-    fetch('https://africaistalking.herokuapp.com/api/sms',
-            {
-                method:'Post',
-                headers:{ apikey, 'Content-Type':"Application/json" },
-                body: JSON.stringify({ username, to, message,from })
-            })
-        .then((res)=>res.json()))
-
-export default sendMessage
+sendMessage(username,message,to,apikey,from){
+    return fetch('https://africaistalking.herokuapp.com/api/sms',
+                {
+                    method:'Post',
+                    headers:{ apikey, 'Content-Type':"Application/json" },
+                    body: JSON.stringify({ username, to, message,from })
+                })
+            .then(function (res){ res.json()})
+    }
+    
+    
+    export default sendMessage
