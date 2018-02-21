c3513ad4d911c162d2d2d7fb62447235db17054d
johnanisere <johnanisere@gmail.com>
2018-02-21 17:51:53 +0100

    re-write to es 5

--- africaistalking.js
+++ africaistalking.js
@@ -1,12 +1,16 @@
-sendMessage(username,message,to,apikey,from){
-    return fetch('https://africaistalking.herokuapp.com/api/sms',
-                {
-                    method:'Post',
-                    headers:{ apikey, 'Content-Type':"Application/json" },
-                    body: JSON.stringify({ username, to, message,from })
-                })
-            .then(function (res){ res.json()})
-    }
-    
-    
-    export default sendMessage
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var sendMessage = function sendMessage(username, message, to, apikey, from) {
+    return fetch('https://africaistalking.herokuapp.com/api/sms', {
+        method: 'Post',
+        headers: { apikey: apikey, 'Content-Type': "Application/json" },
+        body: JSON.stringify({ username: username, to: to, message: message, from: from })
+    }).then(function (res) {
+        return res.json();
+    });
+};
+
+exports.default = sendMessage;
